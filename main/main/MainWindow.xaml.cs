33e8c6858cb1086c398831627a561d6d7ca60071
wayne54515 <0920005wayne@gmail.com>
2019-07-19 23:43:39 +0800

    Postorder/Binary Ans/Decimal Ans撰寫
    prepost未完成

--- main/main/MainWindow.xaml.cs
+++ main/main/MainWindow.xaml.cs
@@ -20,93 +20,213 @@ namespace main
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+        }  
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            if (input_text.Text == "0")
+                input_text.Clear();
+            input_text.Text = input_text.Text + button.Content;
+            Postorder();
+            Preorder();
         }
 
-        
-
-        private void Zero_Click(object sender, RoutedEventArgs e)
+        private void Operate_Click(object sender, RoutedEventArgs e)
         {
-            input_text.AppendText("0");
-        }
-
-        private void One_Click(object sender, RoutedEventArgs e)
-        {
-            
-            input_text.AppendText("1");
-        }
-
-        private void Two_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("2");
-        }
-
-        private void Three_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("3");
-        }
-
-        private void Four_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("4");
-        }
-
-        private void Five_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("5");
-        }
-
-        private void Six_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("6");
-        }
-
-        private void Seven_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("7");
-        }
-
-        private void Eight_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("8");
-        }
-
-        private void Nine_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("9");
-        }
-
-        private void Divide_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("/");
-        }
-
-        private void Multiple_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("*");
-        }
-
-        private void Sub_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("-");
-        }
-
-        private void Add_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.AppendText("+");
+            if (input_text.Text.Length > 0 )
+            {
+                Button button = (Button)sender;
+                input_text.Text = input_text.Text + button.Content;
+                Postorder();
+                Preorder();
+            }
         }
 
         private void Backspace_Click(object sender, RoutedEventArgs e)
-        {
-            input_text.Undo();
+        { 
+            if(input_text.Text.Length > 0)
+            {
+                if (input_text.Text != "0")
+                {
+                    input_text.Undo();
+                    if(input_text.Text.Length == 0)
+                        input_text.Text = "0";
+                    Postorder();
+                    Preorder();
+                }
+            }
+            
         }
 
         private void Ac_Click(object sender, RoutedEventArgs e)
         {
             input_text.Clear();
+            input_text.Text = "0";
+            Postorder();
+            Preorder();
         }
 
+        private void Postorder()
+        {
+            string[] stack = new string[100];
+            int top = -1;
+            if (postorder_result.Text == null)
+                postorder_result.Text = "0";
+            else
+            {
+                postorder_result.Clear();
+                string inorder = input_text.Text;
+                char[] inorder_array = inorder.ToArray();
+                for (var i = 0; i < inorder.Length; i++)
+                {
+                    if (inorder_array[i] == '+')
+                    {
+                        postorder_result.Text = postorder_result.Text + " ";
+                        for(var j=top; j>-1; j--)
+                        {
+                            if((stack[j] == "*")|(stack[j] == "/"))
+                            {
+                                postorder_result.Text = postorder_result.Text + stack[j] + " ";
+                                top--;
+                            }
+                        }
+                        stack[++top] = "+";                   
+                    }
+                    else if (inorder_array[i] == '-')
+                    {
+                        postorder_result.Text = postorder_result.Text + " ";
+                        for (var j = top; j > -1; j--)
+                        {
+                            if ((stack[j] == "*") | (stack[j] == "/"))
+                            {
+                                postorder_result.Text = postorder_result.Text + stack[j] + " ";
+                                top--;
+                            }
+                        }
+                        stack[++top] = "-";
+                    }
+                    else if (inorder_array[i] == '*')
+                    {
+                        postorder_result.Text = postorder_result.Text + " ";
+                        stack[++top] = "*";
+                    }
+                    else if (inorder_array[i] == '/')
+                    {
+                        postorder_result.Text = postorder_result.Text + " ";
+                        stack[++top] = "/";
+                    }
+                    else
+                        postorder_result.Text = postorder_result.Text + inorder_array[i];
+                }
+                for (var i = top; i > -1; i--)
+                {
+                    postorder_result.Text = postorder_result.Text + " " + stack[i] ;
+                    top--;
+                }
+            }
+            Calculate();
+        }
+
+        private void Calculate()
+        {
+            int[] stack = new int[100];
+            int top = -1;
+            string postorder = postorder_result.Text;
+            string[] postorder_array = postorder.Split(' ');
+            foreach (var word in postorder_array)
+            {
+                if ((word == "+") | (word == "-") | (word == "*") | (word == "/") | (word == ""))
+                { 
+                    if ((word == "+") & (top > 0))
+                    {
+                        stack[top - 1] += stack[top--];
+                    }
+                    else if ((word == "-") & (top > 0))
+                    {
+                        stack[top - 1] -= stack[top--];
+                    }
+                    else if ((word == "*") & (top > 0))
+                    {
+                        stack[top - 1] *= stack[top--];
+                    }
+                    else if ((word == "/") & (top > 0))
+                    {
+                        stack[top - 1] = Convert.ToInt32(stack[top - 1] / stack[top--]);
+                    }
+                }
+                else
+                {
+                    stack[++top] = int.Parse(word);
+                }
+            }
+            decimal_result.Text = stack[0].ToString();
+            binary_result.Text = Convert.ToString(stack[0], 2);
+        }
+
+        private void Preorder()
+        {
+            string[] stack = new string[100];
+            int top = -1;
+            if (preorder_result.Text == null)
+                preorder_result.Text = "0";
+            else
+            {
+                preorder_result.Clear();
+                string inorder = input_text.Text;
+                char[] inorder_array = inorder.ToArray();
+                for (var i = 0; i < inorder.Length; i++)
+                {
+                    if (inorder_array[i] == '+')
+                    {
+                        preorder_result.Text = preorder_result.Text + " ";
+                        for (var j = top; j > -1; j--)
+                        {
+                            if ((stack[j] == "*") | (stack[j] == "/"))
+                            {
+                                preorder_result.Text = preorder_result.Text + stack[j] + " ";
+                                top--;
+                            }
+                        }
+                        stack[++top] = "+";
+                    }
+                    else if (inorder_array[i] == '-')
+                    {
+                        preorder_result.Text = preorder_result.Text + " ";
+                        for (var j = top; j > -1; j--)
+                        {
+                            if ((stack[j] == "*") | (stack[j] == "/"))
+                            {
+                                preorder_result.Text = preorder_result.Text + stack[j] + " ";
+                                top--;
+                            }
+                        }
+                        stack[++top] = "-";
+                    }
+                    else if (inorder_array[i] == '*')
+                    {
+                        preorder_result.Text = preorder_result.Text + " ";
+                        stack[++top] = "*";
+                    }
+                    else if (inorder_array[i] == '/')
+                    {
+                        preorder_result.Text = preorder_result.Text + " ";
+                        stack[++top] = "/";
+                    }
+                    else
+                        preorder_result.Text = preorder_result.Text + inorder_array[i];
+                }
+                for (var i = top; i > -1; i--)
+                {
+                    preorder_result.Text = preorder_result.Text + " " + stack[i];
+                    top--;
+                }
+            }
+        }
     }
+
 }

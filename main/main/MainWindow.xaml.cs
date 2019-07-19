f9978a6671f6652356c3b5d368045f03aa8dd63f
wayne54515 <0920005wayne@gmail.com>
2019-07-19 18:10:04 +0800

    畫面設計&按鍵功能
    計算未完成
    前序/後續未完成

--- main/main/MainWindow.xaml.cs
+++ main/main/MainWindow.xaml.cs
@@ -24,5 +24,89 @@ namespace main
         {
             InitializeComponent();
         }
+
+        
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("0");
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            
+            input_text.AppendText("1");
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("2");
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("3");
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("4");
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("5");
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("6");
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("7");
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("8");
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("9");
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("/");
+        }
+
+        private void Multiple_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("*");
+        }
+
+        private void Sub_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("-");
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.AppendText("+");
+        }
+
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.Undo();
+        }
+
+        private void Ac_Click(object sender, RoutedEventArgs e)
+        {
+            input_text.Clear();
+        }
+
     }
 }
